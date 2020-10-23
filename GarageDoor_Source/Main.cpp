8a692d71635fc2133114867c7308fa00810ddbf0
LoneDev6 <leoxy98@gmail.com>
2020-10-23 19:06:21 +0200

    License

--- GarageDoor_Source/Main.cpp
+++ GarageDoor_Source/Main.cpp
@@ -1,4 +1,24 @@
-﻿#pragma once
+﻿/*
+LoneDev super professional License
+
+
+Copyright (c) 2020 LoneDev
+
+
+Permissions:
+- You can edit this source and use it in any free project you want
+
+Limitations:
+- You cannot use this source or edited versions in commercial projects
+- You cannot use this code in projects that earn using advertisements or that take donations
+- You cannot resell this code
+- You cannot state that this is your code
+- You must include LoneDev in the credits of your project
+- You must share the edited source code
+- You must include this license file
+*/
+
+#pragma once
 
 #include <thread>
 
